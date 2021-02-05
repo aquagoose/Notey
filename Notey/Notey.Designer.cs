65eac2bc10ad0a0b930d2f223a21a2e8d2b141d2
Ollie Robinson <ollie@ollierobinson.co.uk>
2021-02-05 22:44:08 +0000

    Added stuff and things

--- Notey/Notey.Designer.cs
+++ Notey/Notey.Designer.cs
@@ -38,24 +38,33 @@
             this.fileSave = new System.Windows.Forms.ToolStripMenuItem();
             this.fileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
             this.fileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.formatWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateButton = new System.Windows.Forms.ToolStripButton();
             this.toolBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox
             // 
+            this.textBox.AcceptsTab = true;
             this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textBox.Location = new System.Drawing.Point(0, 43);
+            this.textBox.EnableAutoDragDrop = true;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.textBox.Location = new System.Drawing.Point(0, 28);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(800, 407);
+            this.textBox.Size = new System.Drawing.Size(800, 422);
             this.textBox.TabIndex = 0;
             this.textBox.Text = "";
+            this.textBox.WordWrap = false;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // toolBar
             // 
             this.toolBar.BackColor = System.Drawing.SystemColors.Window;
             this.toolBar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileButton});
+            this.toolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileButton, this.formatButton, this.updateButton});
             this.toolBar.Location = new System.Drawing.Point(0, 0);
             this.toolBar.Name = "toolBar";
             this.toolBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
@@ -77,35 +86,74 @@
             // fileNew
             // 
             this.fileNew.Name = "fileNew";
-            this.fileNew.Size = new System.Drawing.Size(152, 22);
+            this.fileNew.Size = new System.Drawing.Size(114, 22);
             this.fileNew.Text = "New";
             // 
             // fileOpen
             // 
             this.fileOpen.Name = "fileOpen";
-            this.fileOpen.Size = new System.Drawing.Size(152, 22);
+            this.fileOpen.Size = new System.Drawing.Size(114, 22);
             this.fileOpen.Text = "Open";
             this.fileOpen.Click += new System.EventHandler(this.fileOpen_Click);
             // 
             // fileSave
             // 
             this.fileSave.Name = "fileSave";
-            this.fileSave.Size = new System.Drawing.Size(152, 22);
+            this.fileSave.Size = new System.Drawing.Size(114, 22);
             this.fileSave.Text = "Save";
             this.fileSave.Click += new System.EventHandler(this.fileSave_Click);
             // 
             // fileSaveAs
             // 
             this.fileSaveAs.Name = "fileSaveAs";
-            this.fileSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.fileSaveAs.Size = new System.Drawing.Size(114, 22);
             this.fileSaveAs.Text = "Save As";
             this.fileSaveAs.Click += new System.EventHandler(this.fileSaveAs_Click);
             // 
             // fileExit
             // 
             this.fileExit.Name = "fileExit";
-            this.fileExit.Size = new System.Drawing.Size(152, 22);
+            this.fileExit.Size = new System.Drawing.Size(114, 22);
             this.fileExit.Text = "Exit";
+            this.fileExit.Click += new System.EventHandler(this.fileExit_Click);
+            // 
+            // formatButton
+            // 
+            this.formatButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.formatButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.formatWordWrap, this.formatFont});
+            this.formatButton.Image = ((System.Drawing.Image) (resources.GetObject("formatButton.Image")));
+            this.formatButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.formatButton.Name = "formatButton";
+            this.formatButton.ShowDropDownArrow = false;
+            this.formatButton.Size = new System.Drawing.Size(49, 22);
+            this.formatButton.Text = "Format";
+            // 
+            // formatWordWrap
+            // 
+            this.formatWordWrap.CheckOnClick = true;
+            this.formatWordWrap.Name = "formatWordWrap";
+            this.formatWordWrap.Size = new System.Drawing.Size(134, 22);
+            this.formatWordWrap.Text = "Word Wrap";
+            this.formatWordWrap.CheckedChanged += new System.EventHandler(this.formatWordWrap_CheckedChanged);
+            // 
+            // formatFont
+            // 
+            this.formatFont.Name = "formatFont";
+            this.formatFont.Size = new System.Drawing.Size(134, 22);
+            this.formatFont.Text = "Font...";
+            this.formatFont.Click += new System.EventHandler(this.formatFont_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.updateButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.updateButton.Image = ((System.Drawing.Image) (resources.GetObject("updateButton.Image")));
+            this.updateButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(23, 22);
+            this.updateButton.Text = "Update Available!";
+            this.updateButton.Visible = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
             // 
             // Notey
             // 
@@ -115,15 +163,25 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.toolBar);
             this.Controls.Add(this.textBox);
+            this.KeyPreview = true;
             this.Name = "Notey";
-            this.Text = "Notey - There should be text here. Hmmmmm";
+            this.Text = "Notey - If you can see this, something has gone wrong...";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Notey_FormClosing);
             this.Load += new System.EventHandler(this.Notey_Load);
+            this.Shown += new System.EventHandler(this.Notey_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Notey_KeyDown);
             this.Resize += new System.EventHandler(this.Notey_Resize);
             this.toolBar.ResumeLayout(false);
             this.toolBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripButton updateButton;
+
+        private System.Windows.Forms.ToolStripMenuItem formatFont;
+        private System.Windows.Forms.ToolStripDropDownButton formatButton;
+        private System.Windows.Forms.ToolStripMenuItem formatWordWrap;
 
         private System.Windows.Forms.ToolStripMenuItem fileExit;
         private System.Windows.Forms.ToolStripMenuItem fileNew;
--- Notey/Notey.cs
+++ Notey/Notey.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace Notey
 {
     public partial class Notey : Form
     {
+        private const string Version = "0.00"; // The current version number of the software.
+        
+        private const string UpdatePath = "https://www.ollierobinson.co.uk/Notey/UpdateInfo.json";
+        private const string DownloadPath = "https://github.com/ohtrobinson/Notey";
+        
         private string _filePath = String.Empty; // The full path of the file (C:\Users\user\Documents\untitled.not)
         private string _fileName = String.Empty; // The name of the file (untitled.not)
 
@@ -23,7 +26,7 @@ namespace Notey
             set
             {
                 _fileName = value;
-                Text = (_fileEdited ? "*" : "") + value + " - Notey";
+                Text = (_fileEdited ? "*" : "") + value + " - Notey"; // Set the title of notey.
             }
         }
 
@@ -40,61 +43,113 @@ namespace Notey
             {
                 SaveFileDialog saveFileDialog = new() // Create a new save dialog.
                 {
-                    Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+                    Filter = "Notey documents (*.not)|*.not|Rich-text format documents (*.rtf)|*.rtf|Text files (*.txt)|*.txt|All files (*.*)|*.*"
                 };
                 saveFileDialog.ShowDialog();
                 if (saveFileDialog.FileName != "")
                 {
                     _filePath = saveFileDialog.FileName;
                     _fileEdited = false;
-                    FileName = saveFileDialog.FileName;
-                    textBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                    FileName = _filePath.Split(@"\").Last();
+                    textBox.SaveFile(saveFileDialog.FileName, saveFileDialog.FileName.EndsWith(".not") || saveFileDialog.FileName.EndsWith(".rtf") ? RichTextBoxStreamType.RichText : RichTextBoxStreamType.PlainText);
                 }
             }
             else
             {
                 _filePath = filePath;
                 _fileEdited = false;
-                FileName = filePath;
-                textBox.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                FileName = _fileName;
+                textBox.SaveFile(filePath, filePath.EndsWith(".not") || filePath.EndsWith(".rtf") ? RichTextBoxStreamType.RichText : RichTextBoxStreamType.PlainText);
             }
         }
 
         private void Open(string filePath = null)
         {
+            if (_fileEdited)
+            {
+                DialogResult result = SaveChangesPrompt();
+                if (result == DialogResult.Cancel) return;
+                if (result == DialogResult.Yes) Save(_filePath);
+            }
             if (String.IsNullOrWhiteSpace(filePath))
             {
                 OpenFileDialog openFileDialog = new()
                 {
-                    Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+                    Filter = "Openable files (*.not;*.rtf;*.txt)|*.not;*.rtf;*.txt|(Notey documents (*.not)|*.not|Rich-text format documents (*.rtf)|*.rtf|Text files (*.txt)|*.txt|All files (*.*)|*.*"
                 };
                 openFileDialog.ShowDialog();
                 if (openFileDialog.FileName != "")
                 {
+                    textBox.LoadFile(openFileDialog.FileName, openFileDialog.FileName.EndsWith(".not") || openFileDialog.FileName.EndsWith(".rtf") ? RichTextBoxStreamType.RichText : RichTextBoxStreamType.PlainText);
                     _filePath = openFileDialog.FileName;
                     _fileEdited = false;
                     FileName = openFileDialog.SafeFileName;
-                    textBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
                 }
             }
             else
             {
+                textBox.LoadFile(_filePath, _filePath.EndsWith(".not") || _filePath.EndsWith(".rtf") ? RichTextBoxStreamType.RichText : RichTextBoxStreamType.PlainText);
                 _filePath = filePath;
                 _fileEdited = false;
-                FileName = filePath;
-                textBox.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                FileName = _filePath.Split(@"\").Last();
             }
+        }
+
+        private DialogResult SaveChangesPrompt()
+        {
+            DialogResult result = MessageBox.Show($"Would you like to save changes to {_fileName}?", "Notey",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            return result;
+        }
+
+        private async Task CheckForUpdates() // Check for updates.
+        {
+            try
+            {
+                HttpClient client = new();
+                string content = await client.GetStringAsync(UpdatePath); // Look for this website.
+                string version = (string) JObject.Parse(content)["version"];
+                if (version != Version)
+                {
+                    DialogResult result =
+                        MessageBox.Show("An update is available for Notey. Would you like to download it?",
+                            "Update Available!",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (result == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo("cmd", $"/c start {DownloadPath}")
+                                {CreateNoWindow = true});
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.ToString(), "An error has occured.");
+                        }
+                    }
+
+                    updateButton.Visible = true;
+                }
+            }
+            catch (Exception) { }
         }
         
         #endregion
         public Notey(string[] args)
         {
             InitializeComponent();
+            FileName = "Untitled";
+            if (args.Length > 0) Open(args[0]);
         }
 
         private void Notey_Load(object sender, EventArgs e) // When Notey loads, set the text box to the correct size.
         {
             ResizeTextBox();
+        }
+        
+        private void Notey_Shown(object sender, EventArgs e)
+        {
+            CheckForUpdates();
         }
 
         private void Notey_Resize(object sender, EventArgs e) // When Notey is resized, set the text box to the new correct size.
@@ -115,6 +170,65 @@ namespace Notey
         private void fileOpen_Click(object sender, EventArgs e) // When the open button is clicked
         {
             Open();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e) // Run whenever the text box text is changed.
+        {
+            if (_fileEdited) return;
+            _fileEdited = true;
+            FileName = _fileName;
+        }
+
+        private void Notey_FormClosing(object sender, FormClosingEventArgs e) // Run whenever the form is closing.
+        {
+            if (!_fileEdited) return; // If the file has not been edited, just close the form.
+            DialogResult result = SaveChangesPrompt(); // Otherwise, send a prompt asking if the user wants to save changes.
+            if (result == DialogResult.Cancel) e.Cancel = true;
+            if (result == DialogResult.Yes) Save();
+        }
+
+        private void fileExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Notey_KeyDown(object sender, KeyEventArgs e) // Deals with the keypresses
+        {
+            if ((e.Modifiers & Keys.Control) != 0) // This section only responds to the ctrl key.
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.S: // Save
+                        Save(_filePath);
+                        break;
+                    case Keys.O: // Open
+                        Open();
+                        break;
+                }
+            }
+        }
+
+        private void formatWordWrap_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox.WordWrap = formatWordWrap.Checked;
+        }
+
+        private void formatFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new()
+            {
+                Font = textBox.Font,
+                ShowEffects = false
+            };
+            if (fontDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                textBox.Font = fontDialog.Font;
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            CheckForUpdates();
         }
     }
 }
