eb49e5f2a6963cc723a7f0ee854c098424d965c1
Ollie Robinson <ollie@ollierobinson.co.uk>
2021-02-05 16:47:36 +0000

    Initial commit

---
+++ Notey/Notey.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Notey
+{
+    partial class Notey
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Notey));
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.toolBar = new System.Windows.Forms.ToolStrip();
+            this.fileButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.fileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox.Location = new System.Drawing.Point(0, 43);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(800, 407);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            // 
+            // toolBar
+            // 
+            this.toolBar.BackColor = System.Drawing.SystemColors.Window;
+            this.toolBar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileButton});
+            this.toolBar.Location = new System.Drawing.Point(0, 0);
+            this.toolBar.Name = "toolBar";
+            this.toolBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolBar.Size = new System.Drawing.Size(800, 25);
+            this.toolBar.TabIndex = 1;
+            this.toolBar.Text = "toolStrip1";
+            // 
+            // fileButton
+            // 
+            this.fileButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileNew, this.fileOpen, this.fileSave, this.fileSaveAs, this.fileExit});
+            this.fileButton.Image = ((System.Drawing.Image) (resources.GetObject("fileButton.Image")));
+            this.fileButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fileButton.Name = "fileButton";
+            this.fileButton.ShowDropDownArrow = false;
+            this.fileButton.Size = new System.Drawing.Size(29, 22);
+            this.fileButton.Text = "File";
+            // 
+            // fileNew
+            // 
+            this.fileNew.Name = "fileNew";
+            this.fileNew.Size = new System.Drawing.Size(152, 22);
+            this.fileNew.Text = "New";
+            // 
+            // fileOpen
+            // 
+            this.fileOpen.Name = "fileOpen";
+            this.fileOpen.Size = new System.Drawing.Size(152, 22);
+            this.fileOpen.Text = "Open";
+            this.fileOpen.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // fileSave
+            // 
+            this.fileSave.Name = "fileSave";
+            this.fileSave.Size = new System.Drawing.Size(152, 22);
+            this.fileSave.Text = "Save";
+            this.fileSave.Click += new System.EventHandler(this.fileSave_Click);
+            // 
+            // fileSaveAs
+            // 
+            this.fileSaveAs.Name = "fileSaveAs";
+            this.fileSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.fileSaveAs.Text = "Save As";
+            this.fileSaveAs.Click += new System.EventHandler(this.fileSaveAs_Click);
+            // 
+            // fileExit
+            // 
+            this.fileExit.Name = "fileExit";
+            this.fileExit.Size = new System.Drawing.Size(152, 22);
+            this.fileExit.Text = "Exit";
+            // 
+            // Notey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolBar);
+            this.Controls.Add(this.textBox);
+            this.Name = "Notey";
+            this.Text = "Notey - There should be text here. Hmmmmm";
+            this.Load += new System.EventHandler(this.Notey_Load);
+            this.Resize += new System.EventHandler(this.Notey_Resize);
+            this.toolBar.ResumeLayout(false);
+            this.toolBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ToolStripMenuItem fileExit;
+        private System.Windows.Forms.ToolStripMenuItem fileNew;
+        private System.Windows.Forms.ToolStripMenuItem fileOpen;
+        private System.Windows.Forms.ToolStripMenuItem fileSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem fileSave;
+        private System.Windows.Forms.ToolStripDropDownButton fileButton;
+
+        private System.Windows.Forms.ToolStrip toolBar;
+
+        private System.Windows.Forms.RichTextBox textBox;
+
+        #endregion
+    }
+}
---
+++ Notey/Notey.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notey
+{
+    public partial class Notey : Form
+    {
+        private string _filePath = String.Empty; // The full path of the file (C:\Users\user\Documents\untitled.not)
+        private string _fileName = String.Empty; // The name of the file (untitled.not)
+
+        private bool _fileEdited = false; // Has the file been edited?
+
+        private string FileName
+        {
+            get => _fileName;
+            set
+            {
+                _fileName = value;
+                Text = (_fileEdited ? "*" : "") + value + " - Notey";
+            }
+        }
+
+        #region Reusable Methods
+
+        private void ResizeTextBox() // Resize the text box so that it fits correctly.
+        {
+            textBox.Height = ClientSize.Height - toolBar.Height;
+        }
+
+        private void Save(string filePath = null) // Save a file.
+        {
+            if (string.IsNullOrWhiteSpace(filePath)) // If there is no file path provided, open the save file dialog.
+            {
+                SaveFileDialog saveFileDialog = new() // Create a new save dialog.
+                {
+                    Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+                };
+                saveFileDialog.ShowDialog();
+                if (saveFileDialog.FileName != "")
+                {
+                    _filePath = saveFileDialog.FileName;
+                    _fileEdited = false;
+                    FileName = saveFileDialog.FileName;
+                    textBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+            else
+            {
+                _filePath = filePath;
+                _fileEdited = false;
+                FileName = filePath;
+                textBox.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void Open(string filePath = null)
+        {
+            if (String.IsNullOrWhiteSpace(filePath))
+            {
+                OpenFileDialog openFileDialog = new()
+                {
+                    Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+                };
+                openFileDialog.ShowDialog();
+                if (openFileDialog.FileName != "")
+                {
+                    _filePath = openFileDialog.FileName;
+                    _fileEdited = false;
+                    FileName = openFileDialog.SafeFileName;
+                    textBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+            else
+            {
+                _filePath = filePath;
+                _fileEdited = false;
+                FileName = filePath;
+                textBox.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+            }
+        }
+        
+        #endregion
+        public Notey(string[] args)
+        {
+            InitializeComponent();
+        }
+
+        private void Notey_Load(object sender, EventArgs e) // When Notey loads, set the text box to the correct size.
+        {
+            ResizeTextBox();
+        }
+
+        private void Notey_Resize(object sender, EventArgs e) // When Notey is resized, set the text box to the new correct size.
+        {
+            ResizeTextBox();
+        }
+
+        private void fileSaveAs_Click(object sender, EventArgs e) // When the save as button is clicked
+        {
+            Save();
+        }
+
+        private void fileSave_Click(object sender, EventArgs e) // When the save button is clicked
+        {
+            Save(_filePath);
+        }
+
+        private void fileOpen_Click(object sender, EventArgs e) // When the open button is clicked
+        {
+            Open();
+        }
+    }
+}
